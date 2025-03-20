781e0d8b2429736d42c77b9e59f9342be5fc7782
Julio Leon <juliopena143@icloud.com>
2025-03-20 13:09:30 -0600

    brain refresh stacks

---
+++ stacks/EXERCISE-Stack-Constructor.py
@@ -0,0 +1,30 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        
+# class Stack:
+    ## WRITE STACK CONSTRUCTOR HERE ##
+    #                                #
+    #                                #
+    #                                #
+    #                                #
+    ##################################
+
+
+
+
+my_stack = Stack(4)
+
+print('Top:', my_stack.top.value)
+print('Height:', my_stack.height)
+
+
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    Top: 4
+    Height: 1
+
+"""
